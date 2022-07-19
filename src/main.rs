d725019a2a5af549237e5da64e324cb9a502ef2e
zkud <anton.kudyrka@gmail.com>
2022-07-19 11:15:53 +0300

    Fixed typos and added a small hint

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,14 @@ use std::env;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
+  if args.len() < 2 {
+    println!("CIM 0.2.0");
+    println!("CDS Implementation generation by a Metadata document");
+    println!("Incorrect usage path arg is missing, try cim <filename> instead");
+    return;
+  }
   let filename = &args[1];
-  println!("CIM 0.1.0");
+  println!("CIM 0.2.0");
   println!("Reading {filename}...");
   let cds = parser::parse(filename);
   print!("{cds}");
