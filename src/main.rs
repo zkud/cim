f7865398668ec817e71e7f3938e607a6d7aec0e2
zkud <anton.kudyrka@gmail.com>
2022-07-30 21:43:52 +0300

    Version update

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 
 fn main() {
   let args = Args::parse();
-  println!("CIM 0.2.4");
+  println!("CIM 0.2.5");
   println!("Reading {}...", args.path);
   let cds = run(args);
   print!("{cds}");
